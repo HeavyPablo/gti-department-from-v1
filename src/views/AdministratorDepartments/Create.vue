73f7762dd1e2ac05ee19eae0515b97dac01a69cf
ch1cobest1a <bay.plecaros@gmail.com>
2021-10-22 00:33:10 -0300

    xd

--- src/views/AdministratorDepartments/Create.vue
+++ src/views/AdministratorDepartments/Create.vue
@@ -2,39 +2,81 @@
     <div>
         <modal id="dlgNewDepartment" title="Crear nuevo departamento">
             <template v-slot:body>
-                <div class="row">
-                    <div class="form-group col-sm-12 mb-1">
-                        <label class="form-label">Dirección</label>
-                        <input type="text" class="form-control" v-model="create.address"/>
-                    </div>
+                <form>
+                    <div class="row">
+                        <div class="ecommerce-gallery" data-mdb-zoom-effect="true" data-mdb-auto-height="true">
+                            <div class="row py-3 shadow-5">
+                                <div class="col-sm-3 mb-1">
+                                        <div class="lightbox">
+                                            <img src="https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Vertical/14a.jpg"
+                                                 alt="Gallery image 1"
+                                                 class="ecommerce-gallery-main-img active w-100"
+                                            />
+                                        </div>
+                                        <div class="row">
+                                            <div class="col-4 mt-1">
+                                                <img src="https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Vertical/12a.jpg"
+                                                    data-mdb-img="https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Vertical/12a.jpg"
+                                                    alt="Gallery image 2"
+                                                    class="w-100"
+                                                />
+                                            </div>
+                                             <div class="col-4 mt-1">
+                                                <img src="https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Vertical/12a.jpg"
+                                                    data-mdb-img="https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Vertical/12a.jpg"
+                                                    alt="Gallery image 2"
+                                                    class="w-100"
+                                                />
+                                            </div>
+                                            <div class="col-4 mt-1">
+                                                <img src="https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Vertical/12a.jpg"
+                                                    data-mdb-img="https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Vertical/12a.jpg"
+                                                    alt="Gallery image 2"
+                                                    class="w-100"
+                                                />
+                                            </div>
 
-                    <div class="form-group col-sm-12 mb-1">
-                        <label class="form-label">Capacidad: <span class="text-gray-600">{{ create.capacity }}</span></label>
-                        <input type="range" class="form-range" min="0" max="10" v-model="create.capacity"/>
-                    </div>
 
-                    <div class="form-group col-sm-12 mb-1">
-                        <label class="form-label">Dormitorios <span class="text-gray-600">{{ create.bedroom }}</span></label>
-                        <input type="range" class="form-range" min="0" max="10" v-model="create.bedroom"/>
-                    </div>
+                                         </div>
+                                </div>
 
-                    <div class="form-group col-sm-12 mb-1">
-                        <label class="form-label">Baños <span class="text-gray-600">{{ create.bathroom }}</span></label>
-                        <input type="range" class="form-range" min="0" max="10" v-model="create.bathroom"/>
-                    </div>
+                                <div class="col-sm-9">
+                                    <div class="row">
+                                        <div class="col-8 col-sm-6">
+                                            <label class="form-label">Dirección</label>
+                                            <input type="text" class="form-control" v-model="create.address"/>
+                                        </div>
+                                        <div class="col-4 col-sm-6 ">
+                                            <label class="form-label">Valor</label>
+                                            <input type="number" class="form-control" v-model="create.value"/>
+                                        </div>
 
-                    <div class="form-group col-sm-12 mb-1">
-                        <label class="form-label">Valor</label>
-                        <input type="number" class="form-control" v-model="create.value"/>
-                    </div>
+                                        <div class="col-15 col-sm-15 mt-1">
+                                          <label class="form-label">Capacidad: <span class="text-gray-600">{{ create.capacity }}</span></label>
+                                          <input type="range" class="form-range" min="0" max="10" v-model="create.capacity"/>
+                                        </div>
 
-                    <div class="form-group col-sm-12 mb-1">
-                        <label class="form-label">Descripción</label>
-                        <textarea class="form-control" rows="3" v-model="create.description"></textarea>
+                                        <div class="col-15 col-sm-15 mt-1">
+                                            <label class="form-label">Dormitorios <span class="text-gray-600">{{ create.bedroom }}</span></label>
+                                            <input type="range" class="form-range" min="0" max="10" v-model="create.bedroom"/>
+                                        </div>
+
+                                        <div class="col-15 col-sm-15 mt-1">
+                                            <label class="form-label">Baños <span class="text-gray-600">{{ create.bathroom }}</span></label>
+                                            <input type="range" class="form-range" min="0" max="10" v-model="create.bathroom"/>
+                                        </div>
+
+                                        <div class="col-15 col-sm-15 mt-1">
+                                             <label class="form-label">Descripción</label>
+                                             <textarea class="form-control" rows="3" v-model="create.description"></textarea>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>  
+                        </div>     
                     </div>
-                </div>
+                </form>
             </template>
-
             <template v-slot:btnSuccess>
                 <button type="button" class="btn btn-primary" @click="store">Guardar</button>
             </template>
