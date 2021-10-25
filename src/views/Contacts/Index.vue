f8af4e2b56179ee4f7117ea2190abb984e95124c
Diego-Orellana-Rojas <orellanarojasd@gmail.com>
2021-10-24 23:01:05 -0300

    Revert "Merge branch 'master' of https://github.com/HeavyPablo/gti-department-from-v1"
    This reverts commit 75ebca9f0291080e46b9097e09e70476e23c8dad, reversing
    changes made to 0e7ea25e5b0581181c37073d98d32e43db3b5f28.

--- src/views/Contacts/Index.vue
+++ src/views/Contacts/Index.vue
@@ -2,36 +2,36 @@
     <div>
         <div class="card">
             <div class="card-header">
-                <h4 class="card-title">Contacto</h4>
-                <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#dlgNewContact">
+                <h4 class="card-title">Contactos</h4>
+                <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#dlgNewContacts">
                     <vue-feather type="plus" size="1rem"></vue-feather> Agregar
                 </button>
             </div>
 
             <div class="card-body">
-                <table id="contactTable" class="table">
+                <table id="ContactsTable" class="table">
                     <thead>
                         <tr>
                         <th>ID</th>
                         <th>Nombre Completo</th>
-                        <th>Teléfono</th>
-                        <th>Correo</th>
+                        <th>Telefono Celular </th>
+                        <th>Email</th>
                         <th class="no-sort text-end">Acciones</th>
                         </tr>
                     </thead>
                     <tbody>
-                        <tr v-for="(contact, index) in contacts" :key="index">
-                            <td class="align-middle">{{ contact.id }}</td>
-                            <td class="align-middle">{{ contact.full_name }}</td>
-                            <td class="align-middle">{{ contact.phone_number }}</td>
-                            <td class="align-middle">{{ contact.email }}</td>
+                        <tr v-for="(Contacts, index) in Contacts" :key="index">
+                            <td class="align-middle">{{ Contacts.id }}</td>
+                            <td class="align-middle">{{ Contacts.full_name }}</td>
+                            <td class="align-middle">{{ Contacts.phone_number }}</td>
+                            <td class="align-middle">{{ Contacts.email }}</td>
                             <td class="align-middle text-end">
                                 <button type="button" class="btn btn-warning mx-2" 
-                                    data-bs-toggle="modal" data-bs-target="#dlgEditContact" 
-                                    @click="show(contact)">
+                                    data-bs-toggle="modal" data-bs-target="#dlgEditContacts" 
+                                    @click="show(Contacts)">
                                     <vue-feather type="edit-2" size="14"></vue-feather>
                                 </button>
-                                <button type="button" class="btn btn-danger mx-2" @click="destroy(contact.id)">
+                                <button type="button" class="btn btn-danger mx-2" @click="destroy(Contacts.id)">
                                     <vue-feather type="trash-2" size="14"></vue-feather>
                                 </button>
                             </td>
@@ -39,55 +39,57 @@
                     </tbody>
                 </table>
             </div>
+        </div>    
             
-        </div>
+        <contactsCreate @stored="index"></contactsCreate>
 
 
-        <contact-create @stored="index"></contact-create>
-
-
-        <modal id="dlgEditContact" title="Editar contacto">
+        <modal id="dlgEditContacts" title="Editar contactos">
             <template v-slot:body>
-                <div class="row">
+                <div class="row">                 
                     <div class="form-group col-sm-12">
                         <label class="form-label">Nombre</label>
                         <input type="text" class="form-control" v-model="edit.full_name"/>
                     </div>
 
                     <div class="form-group col-sm-12">
-                        <label class="form-label">Teléfono</label>
+                        <label class="form-label">Telefono</label>
                         <input type="text" class="form-control" v-model="edit.phone_number"/>
                     </div>
-
+                    
                     <div class="form-group col-sm-12">
-                        <label class="form-label">Correo</label>
+                        <label class="form-label">Email</label>
                         <input type="text" class="form-control" v-model="edit.email"/>
                     </div>
-
                 </div>
             </template>
 
             <template v-slot:btnSuccess>
                 <button type="button" class="btn btn-primary" @click="update">Guardar</button>
             </template>
-        </modal>     
+        </modal>  
+
+        
+
+
+
     </div>
 </template>
 
 <script>
 
-import Contact from '../../services/Contacts';
-import ContactCreate from './Create';
+import Contacts from '../../services/Contacts';
+import ContactsCreate from './Create';
 import { Modal } from 'bootstrap';
 
 const $ = require('jquery');
 
 export default {
-    components: { ContactCreate },
+    components: { ContactsCreate },
 
     data() {
         return {
-            contacts: [],
+            contacts_type: [],
             edit: {},
             errors: {},
         }
@@ -108,29 +110,29 @@ export default {
                 ]
             };
 
-            $('#contactTable').DataTable().destroy();
+            $('#contactsTable').DataTable().destroy();
 
-            await Contact.get({}, data => {
-                this.contacts = data;
+            await Contacts.get({}, data => {
+                this.contacts_type = data;
 
                 this.$nextTick(() => {
-                    $('#contactTable').DataTable(config);
+                    $('#contactsTable').DataTable(config);
                 })
             });
         },
 
         async update() {
             this.$toast.clear();
-            await Contact.update(this.edit.id, this.edit, () => {
+            await Contacts.update(this.edit.id, this.edit, () => {
 
                 this.edit = {};
 
                 this.$toast.open({
-                    message: 'Tipo de contacto actualizado!',
+                    message: 'Contacto actualizado!',
                     type: 'success'
                 });
 
-                var myModalEl = document.getElementById('dlgEditContact')
+                var myModalEl = document.getElementById('dlgEditContacts')
                 var modal = Modal.getInstance(myModalEl)
                 modal.hide();
 
@@ -146,12 +148,12 @@ export default {
 
         async destroy(element) {
             this.$toast.clear();
-            await Contact.destroy(element, () => {
+            await Contacts.destroy(element, () => {
 
                 this.edit = {};
 
                 this.$toast.open({
-                    message: 'Tipo de contacto eliminado!',
+                    message: 'Contacto eliminado!',
                     type: 'success'
                 });
 
