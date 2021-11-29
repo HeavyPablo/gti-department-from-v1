3267410422ca3d99f3bcd277c664475fbf3cfd59
ch1cobest1a <bay.plecaros@gmail.com>
2021-11-29 11:37:21 -0300

    .

--- src/views/View-Admin/AdministratorServices/Create.vue
+++ src/views/View-Admin/AdministratorServices/Create.vue
@@ -1,45 +1,47 @@
 <template>
     <div>
-        <modal id="dlgNewAdmservice" title="Crear">
+        <modal id="dlgNewAdmservice" title="Crear Servicio">
             <template v-slot:body>
-                <div class="row">
-                        <div class="form-group col-sm-12">
-                            <label class="form-label">Lugar</label>
-                            <input type="text" class="form-control" v-model="create.place"/>
-                        </div>
+                    <div class="col-sm-15">
+                                    <div class="row">
 
-                        <div class="form-group col-sm-12">
-                            <label class="form-label">Desde Fecha</label>
-                            <input type="date" class="form-control" v-model="create.from_date"/>
-                        </div>
+                                        <div class="col-8 col-sm-6">
+                                            <label class="form-label">Lugar</label>
+                                            <input type="text" class="form-control" v-model="create.place"/>
+                                        </div>
 
-                         <div class="form-group col-sm-12">
-                            <label class="form-label">Hasta Fecha</label>
-                            <input type="date" class="form-control" v-model="create.to_date"/>
-                        </div>
+                                        <div class="col-4 col-sm-6 ">
+                                            <label class="form-label">Fecha de ingreso</label>
+                                            <input type="date" class="form-control" v-model="create.from_date"/>
+                                        </div>
 
-                        <div class="form-group col-sm-12">
-                            <label class="form-label">Contacto</label>
-                            <select class="form-control" v-model="create.contacts_id">
-                                <option v-for="contact in contacts" :key="contact.id" :value="contact.id">{{contact.full_name}}</option>
-                            </select>
-                        </div>
+                                        <div class="col-4 col-sm-6 ">
+                                            <label class="form-label">Hasta la fecha</label>
+                                             <input type="date" class="form-control" v-model="create.to_date"/>
+                                        </div>
 
-                        <div class="form-group col-sm-12">
-                            <label class="form-label">Tipo de Servicio</label>
-                            <select class="form-control" v-model="create.services_types_id">
-                                <option v-for="service_type in service_types" :key="service_type.id" :value="service_type.id">{{service_type.name}}</option>
-                            </select>
-                        </div>
+                                         <div class="col-4 col-sm-6 ">
+                                             <label class="form-label">Contacto</label>
+                                            <select class="form-control" v-model="create.contacts_id">
+                                                <option v-for="contact in contacts" :key="contact.id" :value="contact.id">{{contact.full_name}}</option>
+                                            </select>
+                                        </div>
 
-                         <div class="form-group col-sm-12">
-                            <label class="form-label">Transporte</label>
-                            <select class="form-control" v-model="create.transports_id">
-                                <option v-for="transport in transports" :key="transport.id" :value="transport.id">{{transport.vehicle}}</option>
-                            </select>
-                        </div>
+                                         <div class="col-4 col-sm-6 ">
+                                              <label class="form-label">Tipo de Servicio</label>
+                                                <select class="form-control" v-model="create.services_types_id">
+                                                    <option v-for="service_type in service_types" :key="service_type.id" :value="service_type.id">{{service_type.name}}</option>
+                                                </select>
+                                        </div>
 
-                    </div>
+                                         <div class="col-4 col-sm-6 ">
+                                             <label class="form-label">Transporte</label>
+                                            <select class="form-control" v-model="create.transports_id">
+                                                <option v-for="transport in transports" :key="transport.id" :value="transport.id">{{transport.vehicle}}</option>
+                                            </select>
+                                        </div>
+                                    </div>
+                                </div>
             </template>
 
             <template v-slot:btnSuccess>
